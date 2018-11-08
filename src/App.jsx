d158d9bc3bd83178b0360fb75371c282a69f3bc6
James Horton <jameshorton@gmail.com>
2018-11-08 15:16:21 -0500

    completed assessment

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,9 @@ const PHOTO_URL = "https://picsum.photos/200?photo=";
 // This URL can be used to get an array of objects that contain information
 // about various photos.
 const PHOTO_LIST_URL = "https://picsum.photos/list";
-
 class App extends Component {
   // 1. Declare a state object that will be used to track an array of photos
-  state = {
+  state = {photos:[]
   }
 
   // 2. Declare a life cycle method
@@ -17,27 +16,30 @@ class App extends Component {
   //  - will be called after the component is initially rendered
   // - will fetch an array of photos
   // - will add that array of photos to state once received
+
+  async componentDidMount(){
+    let photoList = await fetch(PHOTO_LIST_URL);
+    photoList = await photoList.json();
+    this.setState({photos: photoList});
+    console.log(this.state)
+  }
+
   render() {
     const { photos = [] } = this.state;
     return (
       <React.Fragment>
         <header>
           <h1>Photo Wall</h1>
-          <p>
-            Start by reading App.jsx and completing the numbered steps.
-            Afterward, delete this paragraph. Then, open up App.css and
-            complete the instructions there.
-          </p>
         </header>
         <div className="collage">
             {/* We use map here because Array.prototype.map is an expression,
               * and for loops are not. You'll learn more about this soon! 
               */}
             {photos.map( photo => 
-                <img alt={/* 3. Fill me in with the photo's filename */ ""}
-                     key={/* 4. Fill me in with the photo's id */ ""}
-                     src={/* 5. Fill me in with the photo's URL */ ""}
-                />
+              <img alt={photo.filename}
+              key={photo.id}
+              src={PHOTO_URL + photo.id}
+              />
             )}
         </div>
       </React.Fragment>
